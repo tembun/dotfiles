be4a29100f6f8d472e4242b9e2d461077e20c60b
Artem Bunichev <tembun@bk.ru>
2025-03-17 01:59:38 +0300

    mpv: remove script for pause.

--- .config/mpv/scripts/pus.lua
+++
@@ -1,6 +0,0 @@
-function on_pause_change(name, value)
-	if value == true then mp.set_property("fullscreen", "no")
-	else mp.set_property("fullscreen", "yes")
-	end
-end
-mp.observe_property("pause", "bool", on_pause_change)
