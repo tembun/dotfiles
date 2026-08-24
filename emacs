tembun/dotfiles

commit d1255ac9220c66ef0f10180b1f2b3448d2d253ee

emacs: Add automatic treesitter setup
Emacs 31 introduced (again, just in time, luckily for me) machinery for
automatic setup for treesitter:

* treesit-enabled-modes automatically remaps major modes to their
  treesitter (-ts) versions (if they exist).  I.e. no more need for
  manually remapping the modes.

* treesit-auto-install-grammar automatically installs the treesitter
  parsers for languages.

So sweet!

emacs
@@ -392,6 +392,10 @@ don't have the formatting messed up."
 
 ;; Major modes
 ;;
+;; Treesitter setup
+(setq treesit-enabled-modes t)
+(setq treesit-auto-install-grammar t)
+;;
 ;; C
 (defun c/style ()
   (style/setup-indent)
@@ -414,7 +418,6 @@ don't have the formatting messed up."
 
 ;; sh
 ;;
-(add-to-list 'major-mode-remap-alist '(sh-mode . bash-ts-mode))
 (defun sh/style ()
   (style/setup-indent)
   (setq-local sh-basic-offset style/tab-width)
@@ -429,7 +432,6 @@ don't have the formatting messed up."
 
 ;; JSON
 ;;
-(add-to-list 'major-mode-remap-alist '(js-json-mode . json-ts-mode))
 (defun json/style ()
   (style/setup-indent)
   (setq-local json-ts-mode-indent-offset style/indent-offset))
@@ -437,7 +439,6 @@ don't have the formatting messed up."
 
 ;; JavaScript
 ;;
-(add-to-list 'major-mode-remap-alist '(js-mode . js-ts-mode))
 (defun js/style ()
   (style/setup-indent)
   (setq-local js-indent-level style/indent-offset))
@@ -447,7 +448,6 @@ don't have the formatting messed up."
 
 ;; TypeScript
 ;;
-(add-to-list 'major-mode-remap-alist '(typescript-mode . typescript-ts-mode))
 (add-hook 'typescript-ts-mode-hook #'eglot-ensure)
 (defun ts/style ()
   (style/setup-indent)

commit 8594d46554e45be74f0c6cdddbd4fdd9d0c58054

emacs: Fix treesitter setup
* describe-variable has a note for treesit-enable-modes: `Setting this
  variable directly with setq may not take effect; use either customize
  or setopt.`.

* t: was a wrong value for treesit-auto-install-grammar - it should be
  'always' in my case, otherwise it won't effectively download the
  treesitter parsers and even the *-ts-mode will be set for a buffer, it
  won't work correctly.

Fixes: d1255ac9220c ("emacs: Add automatic treesitter setup")

emacs
@@ -418,8 +418,8 @@ don't have the formatting messed up."
 ;; Major modes
 ;;
 ;; Treesitter setup
-(setq treesit-enabled-modes t)
-(setq treesit-auto-install-grammar t)
+(setopt treesit-enabled-modes t)
+(setq treesit-auto-install-grammar 'always)
 ;; C
 ;;
 (defun c/style ()